412ea097c6f6b180c7146908253146e1f1053d9c
sairam <sairm21@gmail.com>
2023-06-10 20:00:25 +0530

    Adding mongodb installation script

--- Mongodb.sh
+++ Mongodb.sh
@@ -1,11 +1,11 @@
 echo -e "\e[32m Copying mongodb repo files\e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongo.repo
+cp mongodb.repo /etc/yum.repos.d/mongo.repo &>> /dev/null
 
 echo -e "\e[32m Installing mongo db service\e[0m"
-yum install mongodb-org -y
+yum install mongodb-org -y &>> /dev/null
 
 # need to modify the config file
 
 echo -e "\e[32m start and stop mongo db service\e[0m"
-systemctl enable mongod
-systemctl restart mongod
+systemctl enable mongod &>> /dev/null
+systemctl restart mongod &>> /dev/null
