8ca0c6902f1aa7bbd91206c3e8d6825d177b2016
sairam <sairm21@gmail.com>
2023-06-11 10:38:10 +0530

    editing mongod conf with sed

--- Mongodb.sh
+++ Mongodb.sh
@@ -4,6 +4,7 @@ cp mongodb.repo /etc/yum.repos.d/mongo.repo &>> /dev/null
 echo -e "\e[32m Installing mongo db service\e[0m"
 yum install mongodb-org -y &>> /dev/null
 
+echo -e "\e[32m Update mongodb lisen address\e[0m"
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 echo -e "\e[32m start and stop mongo db service\e[0m"
