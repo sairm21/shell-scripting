03814c90c29940d34510ea823c1980fd175aefd4
sairam <sairm21@gmail.com>
2023-06-12 20:15:25 +0530

    Adding MYSQL shell scripts

---
+++ MySQL.sh
@@ -0,0 +1,16 @@
+echo -e "\e[32m disable MySQL 8 version.\e[0m"
+yum module disable mysql -y &>> /tmp/roboshop.log
+
+echo -e "\e[32m Setup the MySQL5.7 repo file.\e[0m"
+cp /home/centos/shell-scripting/mysql.repo /etc/yum.repos.d/mysql.repo &>> /tmp/roboshop.log
+
+echo -e "\e[32m Install MySQL Server.\e[0m"
+yum install mysql-community-server -y &>> /tmp/roboshop.log
+
+echo -e "\e[32m Start  and enable MySQL Service.\e[0m"
+systemctl enable mysqld &>> /tmp/roboshop.log
+systemctl restart mysqld &>> /tmp/roboshop.log
+
+echo -e "\e[32m changing root credentials.\e[0m"
+mysql_secure_installation --set-root-pass RoboShop@1 &>> /tmp/roboshop.log
+
