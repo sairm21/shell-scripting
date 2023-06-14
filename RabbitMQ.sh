73d010a6a6ffccacbf2621afd74ef6aee546fd7b
sairam <sairm21@gmail.com>
2023-06-14 19:50:46 +0530

    Adding rabbitmq shell scripts

---
+++ RabbitMQ.sh
@@ -0,0 +1,16 @@
+echo -e "\e[32m Configure YUM Repos \e[0m"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>> /tmp/roboshop.log
+
+echo -e "\e[32m Configure YUM Repos for RabbitMQ \e[0m"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>> /tmp/roboshop.log
+
+echo -e "\e[32m Install RabbitMQ \e[0m"
+yum install rabbitmq-server -y &>> /tmp/roboshop.log
+
+echo -e "\e[32m Start and enable RabbitMQ Service\e[0m"
+systemctl enable rabbitmq-server &>> /tmp/roboshop.log
+systemctl start rabbitmq-server &>> /tmp/roboshop.log
+
+echo -e "\e[32m creating user and setting permissions\e[0m"
+rabbitmqctl add_user roboshop roboshop123 &>> /tmp/roboshop.log
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> /tmp/roboshop.log
