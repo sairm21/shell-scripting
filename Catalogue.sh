e2ab2afe5df881e6dd6a177b8a1a3c82fca05aa3
sairam <sairm21@gmail.com>
2023-06-18 19:22:42 +0530

    adding new mongo db functions in cart, user and catalogue script

--- Catalogue.sh
+++ Catalogue.sh
@@ -3,12 +3,4 @@ component=catalogue
 
 nodejs
 
-echo -e "${colour} Copying mongodb repo files${nocolour}"
-cp /home/centos/shell-scripting/mongodb.repo /etc/yum.repos.d/mongo.repo &>> ${log_file}
-
-echo -e "${colour} Installing mongodv${nocolour}"
-yum install mongodb-org-shell -y &>> ${log_file}
-
-echo -e "${colour} Load Schema${nocolour}"
-mongo --host mongodb-dev.iamadevopsengineer.tech <${app_dir}/schema/$component.js &>> ${log_file}
-
+mongo_schema_setup
--- User.sh
+++ User.sh
@@ -1,40 +1,8 @@
-echo -e "\e[32m Setup NodeJS repos\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> /tmp/roboshop.log
+source common.sh
+component=user
 
-echo -e "\e[32m Install NodeJS\e[0m"
-yum install nodejs -y &>> /tmp/roboshop.log
+nodejs
 
-echo -e "\e[32m Adding application User\e[0m"
-useradd roboshop &>> /tmp/roboshop.log
+mongo_schema_setup
 
-echo -e "\e[32m setup an app directory\e[0m"
-rm -rf /app &>> /tmp/roboshop.log
-mkdir /app
-
-echo -e "\e[32m Download the application code to created app directory\e[0m"
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>> /tmp/roboshop.log
-cd /app
-unzip /tmp/user.zip &>> /tmp/roboshop.log
-
-echo -e "\e[32m download the dependencies\e[0m"
-cd /app
-npm install &>> /tmp/roboshop.log
-
-echo -e "\e[32m Setup SystemD Catalogue Service\e[0m"
-cp /home/centos/shell-scripting/user.service /etc/systemd/system/user.service &>> /tmp/roboshop.log
-
-echo -e "\e[32m Load and start the service\e[0m"
-systemctl daemon-reload &>> /tmp/roboshop.log
-systemctl enable user &>> /tmp/roboshop.log
-systemctl restart user &>> /tmp/roboshop.log
-
-
-echo -e "\e[32m Copying mongodb repo files\e[0m"
-cp /home/centos/shell-scripting/mongodb.repo /etc/yum.repos.d/mongo.repo &>> /tmp/roboshop.log
-
-echo -e "\e[32m Installing mongodv\e[0m"
-yum install mongodb-org-shell -y &>> /tmp/roboshop.log
-
-echo -e "\e[32m Load Schema\e[0m"
-mongo --host mongodb-dev.iamadevopsengineer.tech </app/schema/user.js &>> /tmp/roboshop.log
 
