1ccd966bc10693bb6f16919ea55cca5b687eb62d
sairam <sairm21@gmail.com>
2023-06-18 11:41:48 +0530

    adding common variables in cart script

--- Cart.sh
+++ Cart.sh
@@ -1,29 +1,32 @@
-echo -e "\e[32m Setup NodeJS repos\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> /tmp/roboshop.log
+source common.sh
+component="cart"
 
-echo -e "\e[32m Install NodeJS\e[0m"
-yum install nodejs -y &>> /tmp/roboshop.log
+echo -e "${colour} Setup NodeJS repos${nocolour}"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> ${log_file}
 
-echo -e "\e[32m Adding application User\e[0m"
-useradd roboshop &>> /tmp/roboshop.log
+echo -e "${colour} Install NodeJS${nocolour}"
+yum install nodejs -y &>> ${log_file}
 
-echo -e "\e[32m setup an app directory\e[0m"
-rm -rf /app &>> /tmp/roboshop.log
-mkdir /app
+echo -e "${colour} Adding application User${nocolour}"
+useradd roboshop &>> ${log_file}
 
-echo -e "\e[32m Download the application code to created app directory\e[0m"
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip &>> /tmp/roboshop.log
-cd /app
-unzip /tmp/cart.zip &>> /tmp/roboshop.log
+echo -e "${colour} setup an app directory${nocolour}"
+rm -rf ${app-dir} &>> ${log_file}
+mkdir ${app-dir}
 
-echo -e "\e[32m download the dependencies\e[0m"
-cd /app
-npm install &>> /tmp/roboshop.log
+echo -e "${colour} Download the application code to created app directory${nocolour}"
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>> ${log_file}
+cd ${app-dir}
+unzip /tmp/${component}.zip &>> ${log_file}
 
-echo -e "\e[32m Setup SystemD Catalogue Service\e[0m"
-cp /home/centos/shell-scripting/cart.service /etc/systemd/system/cart.service &>> /tmp/roboshop.log
+echo -e "${colour} download the dependencies${nocolour}"
+cd ${app-dir}
+npm install &>> ${log_file}
 
-echo -e "\e[32m Load and start the service\e[0m"
-systemctl daemon-reload &>> /tmp/roboshop.log
-systemctl enable cart &>> /tmp/roboshop.log
-systemctl restart cart &>> /tmp/roboshop.log
+echo -e "${colour} Setup SystemD Catalogue Service${nocolour}"
+cp /home/centos/shell-scripting/${component}.service /etc/systemd/system/${component}.service &>> ${log_file}
+
+echo -e "${colour} Load and start the service${nocolour}"
+systemctl daemon-reload &>> ${log_file}
+systemctl enable ${component} &>> ${log_file}
+systemctl restart ${component} &>> ${log_file}
