b8c11111af62af50f67bf177a22c31602fe763fb
sairam <sairm21@gmail.com>
2023-06-18 20:18:12 +0530

    adding funtions in shipping script

--- Shipping.sh
+++ Shipping.sh
@@ -1,35 +1,4 @@
-echo -e "\e[32m install maven \e[0m"
-yum install maven -y &>> /tmp/roboshop.log
+source common.sh
+component=catalogue
 
-echo -e "\e[32m Add application User\e[0m"
-useradd roboshop &>> /tmp/roboshop.log
-
-echo -e "\e[32m setup an app directory\e[0m"
-rm -rf /app &>> /tmp/roboshop.log
-mkdir /app &>> /tmp/roboshop.log
-
-echo -e "\e[32m Download the application code to created app directory\e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>> /tmp/roboshop.log
-cd /app &>> /tmp/roboshop.log
-unzip /tmp/shipping.zip &>> /tmp/roboshop.log
-
-echo -e "\e[32m download the dependencies & build the application\e[0m"
-cd /app &>> /tmp/roboshop.log
-mvn clean package &>> /tmp/roboshop.log
-mv target/shipping-1.0.jar shipping.jar &>> /tmp/roboshop.log
-
-echo -e "\e[32m Setup SystemD Shipping Service\e[0m"
-cp /home/centos/shell-scripting/shipping.service /etc/systemd/system/shipping.service &>> /tmp/roboshop.log
-
-echo -e "\e[32m mysql installation\e[0m"
-yum install mysql -y &>> /tmp/roboshop.log
-
-echo -e "\e[32m Load Schema\e[0m"
-mysql -h mysql-dev.iamadevopsengineer.tech -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> /tmp/roboshop.log
-
-echo -e "\e[32m Load the service\e[0m"
-systemctl daemon-reload &>> /tmp/roboshop.log
-
-echo -e "\e[32m Enable and Start the service\e[0m"
-systemctl enable shipping &>> /tmp/roboshop.log
-systemctl restart shipping &>> /tmp/roboshop.log
+maven
--- common.sh
+++ common.sh
@@ -3,6 +3,31 @@ nocolour="\e[0m"
 log_file="/dev/null"
 app_dir="/app"
 
+app_user(){
+  echo -e "${colour} Adding application User${nocolour}"
+  useradd roboshop &>> ${log_file}
+
+  echo -e "${colour} setup an app directory${nocolour}"
+  rm -rf /app &>> ${log_file}
+  mkdir /app &>> ${log_file}
+
+  echo -e "${colour} Download the application code to created app directory${nocolour}"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>> ${log_file}
+  cd ${app_dir}
+  unzip /tmp/${component.zip &>> ${log_file}
+
+}
+
+systemd_setup() {
+    echo -e "${colour} Setup SystemD $component Service${nocolour}"
+    cp /home/centos/shell-scripting/$component.service /etc/systemd/system/$component.service &>> ${log_file}
+
+    echo -e "${colour} Load and start the service${nocolour}"
+    systemctl daemon-reload &>> ${log_file}
+    systemctl enable $component &>> ${log_file}
+    systemctl start $component &>> ${log_file}
+}
+
 nodejs() {
   echo -e "${colour} Setup NodeJS repos${nocolour}"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> ${log_file}
@@ -10,29 +35,13 @@ nodejs() {
   echo -e "${colour} Install NodeJS${nocolour}"
   yum install nodejs -y &>> ${log_file}
 
-  echo -e "${colour} Adding application User${nocolour}"
-  useradd roboshop &>> ${log_file}
-
-  echo -e "${colour} setup an app directory${nocolour}"
-  rm -rf ${app_dir} &>> ${log_file}
-  mkdir ${app_dir}
-
-  echo -e "${colour} Download the application code to created app directory${nocolour}"
-  curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>> ${log_file}
-  cd ${app_dir}
-  unzip /tmp/$component.zip &>> ${log_file}
+  app_user
 
   echo -e "${colour} download the dependencies${nocolour}"
   cd ${app_dir}
   npm install &>> ${log_file}
 
-  echo -e "${colour} Setup SystemD $component Service${nocolour}"
-  cp /home/centos/shell-scripting/$component.service /etc/systemd/system/$component.service &>> ${log_file}
-
-  echo -e "${colour} Load and start the service${nocolour}"
-  systemctl daemon-reload &>> ${log_file}
-  systemctl enable $component &>> ${log_file}
-  systemctl start $component &>> ${log_file}
+systemd_setup
 
 }
 
@@ -46,4 +55,29 @@ mongo_schema_setup() {
   echo -e "${colour} Load Schema${nocolour}"
   mongo --host mongodb-dev.iamadevopsengineer.tech <${app_dir}/schema/$component.js &>> ${log_file}
 
+}
+
+mysql_schema_setup() {
+    echo -e "${colour} mysql installation${nocolour}"
+    yum install mysql -y &>> ${log_file}
+
+    echo -e "${colour} Load Schema${nocolour}"
+    mysql -h mysql-dev.iamadevopsengineer.tech -uroot -pRoboShop@1 < /app/schema/${component}.sql &>> ${log_file}
+
+}
+
+maven() {
+  echo -e "${colour} install maven ${nocolour}"
+  yum install maven -y &>> ${log_file}
+
+  app_user
+
+  echo -e "${colour} download the dependencies & build the application${nocolour}"
+  cd /app &>> ${log_file}
+  mvn clean package &>> ${log_file}
+  mv target/${component}-1.0.jar ${component}.jar &>> ${log_file}
+
+  systemd_setup
+
+  mysql_schema_setup
 }
