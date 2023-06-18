479c48d861b3bc4e86ca294e59f38e05bdd8804b
sairam <sairm21@gmail.com>
2023-06-18 11:01:40 +0530

    variables in catalogue script

--- Catalogue.sh
+++ Catalogue.sh
@@ -1,39 +1,43 @@
-echo -e "\e[32m Setup NodeJS repos\e[0m"
+component=catalogue
+colour="\e[34m"
+nocolour="\e[0m"
+
+echo -e "{colour} Setup NodeJS repos{nocolour}"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> /dev/null
 
-echo -e "\e[32m Install NodeJS\e[0m"
+echo -e "{colour} Install NodeJS{nocolour}"
 yum install nodejs -y &>> /dev/null
 
-echo -e "\e[32m Adding application User\e[0m"
+echo -e "{colour} Adding application User{nocolour}"
 useradd roboshop &>> /dev/null
 
-echo -e "\e[32m setup an app directory\e[0m"
+echo -e "{colour} setup an app directory{nocolour}"
 rm -rf /app &>> /dev/null
 mkdir /app
 
-echo -e "\e[32m Download the application code to created app directory\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>> /dev/null
+echo -e "{colour} Download the application code to created app directory{nocolour}"
+curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>> /dev/null
 cd /app
-unzip /tmp/catalogue.zip &>> /dev/null
+unzip /tmp/$component.zip &>> /dev/null
 
-echo -e "\e[32m download the dependencies\e[0m"
+echo -e "{colour} download the dependencies{nocolour}"
 cd /app
 npm install &>> /dev/null
 
-echo -e "\e[32m Setup SystemD Catalogue Service\e[0m"
-cp /home/centos/shell-scripting/catalogue.service /etc/systemd/system/catalogue.service &>> /dev/null
+echo -e "{colour} Setup SystemD $component Service{nocolour}"
+cp /home/centos/shell-scripting/$component.service /etc/systemd/system/$component.service &>> /dev/null
 
-echo -e "\e[32m Load and start the service\e[0m"
+echo -e "{colour} Load and start the service{nocolour}"
 systemctl daemon-reload &>> /dev/null
-systemctl enable catalogue &>> /dev/null
-systemctl start catalogue &>> /dev/null
+systemctl enable $component &>> /dev/null
+systemctl start $component &>> /dev/null
 
-echo -e "\e[32m Copying mongodb repo files\e[0m"
+echo -e "{colour} Copying mongodb repo files{nocolour}"
 cp /home/centos/shell-scripting/mongodb.repo /etc/yum.repos.d/mongo.repo &>> /dev/null
 
-echo -e "\e[32m Installing mongodv\e[0m"
+echo -e "{colour} Installing mongodv{nocolour}"
 yum install mongodb-org-shell -y &>> /dev/null
 
-echo -e "\e[32m Load Schema\e[0m"
-mongo --host mongodb-dev.iamadevopsengineer.tech </app/schema/catalogue.js &>> /dev/null
+echo -e "{colour} Load Schema{nocolour}"
+mongo --host mongodb-dev.iamadevopsengineer.tech </app/schema/$component.js &>> /dev/null
 
