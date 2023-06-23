70d45c1c1c5784380cb2f2b71ac4cda65c76d535
sairam <sairm21@gmail.com>
2023-06-23 22:32:03 +0530

    fixing password prompt for shipping and payment

--- Shipping.sh
+++ Shipping.sh
@@ -1,6 +1,6 @@
 source common.sh
 component=shipping
-mysql_root_password = $1
+mysql_root_password=$1
 
 if [ -z "$mysql_root_password"]; then
   echo -e "\e[31m mysql root password is missing \e[0m"
